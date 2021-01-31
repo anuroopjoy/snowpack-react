ac5547ab60bddf6a38811467edc42dcb003b0af9
Anuroop Joy <anuroop.joy@hrblock.com>
2021-01-31 12:33:21 +0530

    snow

---
+++ snowpack.config.js
@@ -0,0 +1,28 @@
+/** @type {import("snowpack").SnowpackUserConfig } */
+module.exports = {
+    mount: {
+        public: '/',
+        src: '/dist'
+    },
+    plugins: [
+        '@snowpack/plugin-react-refresh',
+        '@snowpack/plugin-webpack'
+    ],
+    routes: [
+        /* Enable an SPA Fallback in development: */
+        // {"match": "routes", "src": ".*", "dest": "/index.html"},
+    ],
+    optimize: {
+        /* Example: Bundle your final build: */
+        // "bundle": true,
+    },
+    packageOptions: {
+        polyfillNode: true
+    },
+    devOptions: {
+        /* ... */
+    },
+    buildOptions: {
+        /* ... */
+    }
+};
---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import { useState, useEffect } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+    // Create the count state.
+    const [count, setCount] = useState(0);
+    // Update the count (+1 every second).
+    useEffect(() => {
+        const timer = setTimeout(() => setCount(count + 1), 1000);
+        return () => clearTimeout(timer);
+    }, [count, setCount]);
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <p>
+                    Edit <code>src/App.j</code> and save to reload.
+                </p>
+                <a
+                    className="App-link"
+                    href="https://reactjs.org"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Learn React
+                </a>
+                <p>
+                    Page has been open for <code>{count}</code> seconds.
+                </p>
+            </header>
+        </div>
+    );
+}
+
+export default App;
